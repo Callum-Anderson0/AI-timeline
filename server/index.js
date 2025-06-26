911e905fc7b20abfa57b1c3c0597e145cca898d2
Callum Anderson <cm20cja@leeds.ac.uk>
2025-06-26 13:38:29 +0100

    fixed .env

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path:'../.env'});
 
 const text = "Mark Zuckerberg’s Meta has won the backing of a judge in a copyright lawsuit brought by a group of authors, in the second legal victory for the US artificial intelligence industry this week. The writers, who included Sarah Silverman and Ta-Nehisi Coates, had argued that the Facebook owner had breached copyright law by using their books without permission to train its AI system. The ruling follows a decision on Monday that Anthropic, another major player in the AI field, had not infringed authors’ copyright."
 const express = require('express')
@@ -25,6 +25,7 @@ app.get('/events', (req, res) => {
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
+  console.log(process.env.HF_API_KEY, "huggingface key");
   runNER(text);
   fetchAndStoreNews();  // fetch news and store in DB on startup
 })
